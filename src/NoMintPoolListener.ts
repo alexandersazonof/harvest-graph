d770bbfac3476ac7c7cded9885a71e3b66b5ebfa
alex <alexandersazonov0@gmail.com>
2024-11-05 21:46:55 +0300

    Add rewardPaid history

--- src/NoMintPoolListener.ts
+++ src/NoMintPoolListener.ts
@@ -1,7 +1,16 @@
-import { RewardAdded } from "../generated/NoMintNotifyHelperListener/NoMintRewardPoolContract";
+import { RewardAdded, RewardPaid } from "../generated/NoMintNotifyHelperListener/NoMintRewardPoolContract";
 import { NoMintPoolContract } from "../generated/templates/NoMintPoolListener/NoMintPoolContract";
 import { saveReward } from "./types/Reward";
 import { saveApyReward } from "./types/Apy";
+import { BD_TEN, BI_EVERY_24_HOURS, FARM_TOKEN_MAINNET } from './utils/Constant';
+import { pow } from './utils/MathUtils';
+import { BigDecimal, BigInt, Bytes } from '@graphprotocol/graph-ts';
+import { getPriceForCoin } from './utils/PriceUtils';
+import { RewardPaidEntity, TokenPrice } from '../generated/schema';
+import { loadOrCreateERC20Token } from './types/Token';
+import { loadOrCreatePotPool } from './types/PotPool';
+import { store } from '@graphprotocol/graph-ts';
+import { loadOrCreateNoMintPool } from './types/NoMintRewardPool';
 
 export function handleRewardAdded(event: RewardAdded): void {
   const poolAddress = event.address
@@ -13,4 +22,38 @@ export function handleRewardAdded(event: RewardAdded): void {
 
   saveReward(poolAddress, rewardToken, rewardRate, periodFinish, rewardAmount, event.transaction, event.block)
   saveApyReward(poolAddress, rewardToken, rewardRate, periodFinish, rewardAmount, event.transaction, event.block)
+}
+
+export function handleRewardPaid(event: RewardPaid): void {
+  const rewardPaid = new RewardPaidEntity(Bytes.fromUTF8(`${event.transaction.hash.toHex()}-${event.logIndex.toString()}`));
+  rewardPaid.userAddress = event.params.user.toHexString();
+  rewardPaid.pool = loadOrCreateNoMintPool(event.address, event.block).id;
+  rewardPaid.value = event.params.reward;
+  // price
+  const rewardToken = loadOrCreateERC20Token(FARM_TOKEN_MAINNET);
+  // TODO move logic token price for all project
+  const tokenPriceId = FARM_TOKEN_MAINNET.toHexString();
+  let tokenPrice = TokenPrice.load(tokenPriceId);
+  if (tokenPrice == null) {
+    tokenPrice = new TokenPrice(tokenPriceId);
+    const price = getPriceForCoin(FARM_TOKEN_MAINNET, event.block.number.toI32());
+    let priceBD = BigDecimal.zero();
+
+    if (price.gt(BigInt.zero())) {
+      priceBD = price.divDecimal(pow(BD_TEN, rewardToken.decimals));
+    }
+    tokenPrice.price = priceBD;
+    tokenPrice.timestamp = event.block.timestamp;
+    tokenPrice.save();
+  }
+
+  rewardPaid.price = tokenPrice.price;
+  rewardPaid.token = rewardToken.id;
+  rewardPaid.timestamp = event.block.timestamp;
+  rewardPaid.createAtBlock = event.block.number;
+  rewardPaid.save();
+
+  if (tokenPrice.timestamp.plus(BI_EVERY_24_HOURS).lt(event.block.timestamp)) {
+    store.remove('TokenPrice', tokenPriceId);
+  }
 }
--- src/PotPoolListener.ts
+++ src/PotPoolListener.ts
@@ -1,8 +1,15 @@
-import { RewardAdded } from "../generated/PotNotifyHelperListener/PotPoolContract";
+import { RewardAdded, RewardPaid } from "../generated/PotNotifyHelperListener/PotPoolContract";
 import { PotPoolContract } from "../generated/templates/PotPoolListener/PotPoolContract";
 import { saveReward } from "./types/Reward";
 import { saveApyReward } from "./types/Apy";
 import { handlerLogic } from './debug/HandlerCalculator';
+import { BigDecimal, BigInt, Bytes, store } from '@graphprotocol/graph-ts';
+import { RewardPaidEntity, TokenPrice } from '../generated/schema';
+import { loadOrCreatePotPool } from './types/PotPool';
+import { loadOrCreateERC20Token } from './types/Token';
+import { getPriceForCoin } from './utils/PriceUtils';
+import { pow } from './utils/MathUtils';
+import { BD_TEN, BI_EVERY_24_HOURS } from './utils/Constant';
 
 export function handleRewardAdded(event: RewardAdded): void {
   handlerLogic(event.address.toHexString(), 'RewardAdded', event.transaction, event.block);
@@ -15,4 +22,38 @@ export function handleRewardAdded(event: RewardAdded): void {
 
   saveReward(poolAddress, rewardToken, rewardRate, periodFinish, rewardAmount, event.transaction, event.block)
   saveApyReward(poolAddress, rewardToken, rewardRate, periodFinish, rewardAmount, event.transaction, event.block)
+}
+
+export function handleRewardPaid(event: RewardPaid): void {
+  const rewardPaid = new RewardPaidEntity(Bytes.fromUTF8(`${event.transaction.hash.toHex()}-${event.logIndex.toString()}`));
+  rewardPaid.userAddress = event.params.user.toHexString();
+  rewardPaid.pool = loadOrCreatePotPool(event.address, event.block).id;
+  rewardPaid.value = event.params.reward;
+  // price
+  const rewardToken = loadOrCreateERC20Token(event.params.rewardToken);
+  // TODO move logic token price for all project
+  const tokenPriceId = event.params.rewardToken.toHexString();
+  let tokenPrice = TokenPrice.load(tokenPriceId);
+  if (tokenPrice == null) {
+    tokenPrice = new TokenPrice(tokenPriceId);
+    const price = getPriceForCoin(event.params.rewardToken, event.block.number.toI32());
+    let priceBD = BigDecimal.zero();
+
+    if (price.gt(BigInt.zero())) {
+      priceBD = price.divDecimal(pow(BD_TEN, rewardToken.decimals));
+    }
+    tokenPrice.price = priceBD;
+    tokenPrice.timestamp = event.block.timestamp;
+    tokenPrice.save();
+  }
+
+  rewardPaid.price = tokenPrice.price;
+  rewardPaid.token = rewardToken.id;
+  rewardPaid.timestamp = event.block.timestamp;
+  rewardPaid.createAtBlock = event.block.number;
+  rewardPaid.save();
+
+  if (tokenPrice.timestamp.plus(BI_EVERY_24_HOURS).lt(event.block.timestamp)) {
+    store.remove('TokenPrice', tokenPriceId);
+  }
 }
